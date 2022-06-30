22c14b614df619ef6c92c1df47ee202a2ff1292c
Eduardo Bonamigo <edbmuller@gmail.com>
2022-06-30 01:07:08 -0300

    ADD express, dotenv and prismic set up

--- src/app.js
+++ src/app.js
@@ -1,14 +1,55 @@
-const express = require('express')
+import dotenv from 'dotenv'
+import path from 'path'
+import express from 'express'
+import { fileURLToPath } from 'url'
+import * as prismicH from '@prismicio/helpers'
+import { client } from './config/prismicConfig.js'
+
+dotenv.config()
+
 const app = express()
-const path = require('path')
-const port = 3000
+const port = process.env.PORT || 3000
 
-app.set('views', path.join(__dirname, 'views'))
+// Set Pug as template engine
 app.set('view engine', 'pug')
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => res.render('pages/home'))
-app.get('/about', (req, res) => res.render('pages/about'))
-app.get('/collection', (req, res) => res.render('pages/collection'))
+// Middleware function, runs on every route.
+// It Inject prismic context to the locals so that we can access these in
+// our templates.
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    prismicH
+  }
+  next()
+})
+
+/**
+ * ROUTES
+ */
+
+app.get('/', async (req, res) => {
+  // Here we are retrieving the first document from your API endpoint
+  const response = await client.getSingle('home')
+  console.log(response)
+  res.render('pages/home')
+})
+// app.get('/', (req, res) => res.render('pages/home'))
+
+app.get('/about', async (req, res) => {
+  // Here we are retrieving the first document from your API endpoint
+  const about = await client.getSingle('about')
+  const meta = await client.getSingle('metadata')
+
+  res.render('pages/about', { about, meta })
+})
+// app.get('/about', (req, res) => res.render('pages/about'))
+
+app.get('/collections', (req, res) => res.render('pages/collections'))
 app.get('/details/:uid', (req, res) => res.render('pages/details'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!
+  =======================`)
+})
---
+++ src/config/prismicConfig.js
@@ -0,0 +1,40 @@
+// node-fetch is used to make network requests to the Prismic Rest API.
+// In Node.js Prismic projects, you must provide a fetch method to the
+// Prismic client.
+import fetch from 'node-fetch'
+import * as prismic from '@prismicio/client'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const repoName = 'ed-floema' // Fill in your repository name.
+const accessToken = process.env.PRISMIC_ACCESS_TOKEN // If your repository is private, add an access token.
+
+// The `routes` property is your Route Resolver. It defines how you will
+// structure URLs in your project. Update the types to match the Custom
+// Types in your project, and edit the paths to match the routing in your
+// project.
+const routes = [
+  {
+    type: 'home',
+    path: '/'
+  },
+  {
+    type: 'about',
+    path: '/about'
+  },
+  {
+    type: 'collection',
+    path: '/collections'
+  },
+  {
+    type: 'product',
+    path: '/detail/:uid'
+  }
+]
+
+export const client = prismic.createClient(repoName, {
+  fetch,
+  accessToken,
+  routes
+})
